a5f9eb1b1e2591ba40f814d91112e462b5419b7a
Alex Fraser <alex.d.fraser@gmail.com>
2019-03-10 00:28:50 +1100

    Imported character file from GGJ

--- Sources/arm/GridWalk.hx
+++ Sources/arm/GridWalk.hx
@@ -3,7 +3,10 @@ package arm;
 import armory.trait.physics.RigidBody;
 import iron.math.Vec4;
 
+
 class GridWalk extends iron.Trait {
+	static inline var GRID_SIZE = 0.25;
+
 	public function new() {
 		super();
 		notifyOnUpdate(step);
@@ -17,16 +20,23 @@ class GridWalk extends iron.Trait {
 
 	function applyMovement(move:String) {
 		var vec = new Vec4();
-		if (move == '+y') vec.y = 1;
-		else if (move == '-y') vec.y = -1;
-		else if (move == '+x') vec.x = 1;
-		else if (move == '-x') vec.x = -1;
+		if (move == '+y') vec.y = GRID_SIZE;
+		else if (move == '-y') vec.y = -GRID_SIZE;
+		else if (move == '+x') vec.x = GRID_SIZE;
+		else if (move == '-x') vec.x = -GRID_SIZE;
 		this.object.transform.loc.add(vec);
+		snap_to_grid(this.object.transform.loc);
 		this.object.transform.buildMatrix();
 
 		#if arm_physics
 		var rigidBody = object.getTrait(RigidBody);
 		if (rigidBody != null) rigidBody.syncTransform();
 		#end
+	}
+
+	function snap_to_grid(loc:Vec4) {
+		loc.x = Math.round(loc.x / GRID_SIZE) * GRID_SIZE;
+		loc.y = Math.round(loc.y / GRID_SIZE) * GRID_SIZE;
+		loc.z = Math.round(loc.z / GRID_SIZE) * GRID_SIZE;
 	}
 }
