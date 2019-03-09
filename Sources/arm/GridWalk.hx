b091cf46effa60aad213f7e91b0b5a78f46db644
Alex Fraser <alex.d.fraser@gmail.com>
2019-03-10 00:58:49 +1100

    Snapping to grid cell centres based on orientation

--- Sources/arm/GridWalk.hx
+++ Sources/arm/GridWalk.hx
@@ -2,6 +2,7 @@ package arm;
 
 import armory.trait.physics.RigidBody;
 import iron.math.Vec4;
+import iron.math.Math;
 
 
 class GridWalk extends iron.Trait {
@@ -9,7 +10,13 @@ class GridWalk extends iron.Trait {
 
 	public function new() {
 		super();
+		notifyOnInit(init);
 		notifyOnUpdate(step);
+	}
+
+	function init() {
+		this.object.transform.loc = snap_to_grid(this.object.transform.loc);
+		this.object.transform.buildMatrix();
 	}
 
 	function step() {
@@ -25,7 +32,7 @@ class GridWalk extends iron.Trait {
 		else if (move == '+x') vec.x = GRID_SIZE;
 		else if (move == '-x') vec.x = -GRID_SIZE;
 		this.object.transform.loc.add(vec);
-		snap_to_grid(this.object.transform.loc);
+		this.object.transform.loc = snap_to_grid(this.object.transform.loc);
 		this.object.transform.buildMatrix();
 
 		#if arm_physics
@@ -34,9 +41,26 @@ class GridWalk extends iron.Trait {
 		#end
 	}
 
-	function snap_to_grid(loc:Vec4) {
-		loc.x = Math.round(loc.x / GRID_SIZE) * GRID_SIZE;
-		loc.y = Math.round(loc.y / GRID_SIZE) * GRID_SIZE;
-		loc.z = Math.round(loc.z / GRID_SIZE) * GRID_SIZE;
+	function snap_to_grid(loc:Vec4):Vec4 {
+		var up = this.object.transform.up();
+		var halfStep = GRID_SIZE * 0.5;
+		var origin:Vec4;
+		if (up.x > 0.9 || up.x < -0.9)
+			// Z aligned with world X
+			origin = new Vec4(0, halfStep, halfStep, 0);
+		else if (up.y > 0.9 || up.y < -0.9)
+			// Z aligned with world Y
+			origin = new Vec4(halfStep, 0, halfStep, 0);
+		else
+			// Z aligned with world Z
+			origin = new Vec4(halfStep, halfStep, 0, 0);
+
+		var offsetM = loc.sub(origin);
+		var offsetCells = offsetM.mult(1 / GRID_SIZE);
+		offsetCells.x = Math.round(offsetCells.x);
+		offsetCells.y = Math.round(offsetCells.y);
+		offsetCells.z = Math.round(offsetCells.z);
+		offsetM = offsetCells.mult(GRID_SIZE);
+		return origin.add(offsetM);
 	}
 }
