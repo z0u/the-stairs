8c8f1b6baa020a0de8b7f9f046eea0975549f7e9
Alex Fraser <alex.d.fraser@gmail.com>
2019-03-13 07:47:35 +1100

    Keyboard controls now use tank-style egocentric movement

--- Sources/arm/InputEgo.hx
+++ Sources/arm/InputEgo.hx
@@ -1,5 +1,7 @@
 package arm;
 
+import iron.math.Vec4;
+import iron.math.Math;
 import iron.system.Input;
 
 class InputEgo extends iron.Trait {
@@ -9,13 +11,40 @@ class InputEgo extends iron.Trait {
 	}
 
 	function read() {
-		var keyboard = Input.getKeyboard();
-		var move;
-		if (keyboard.started('up') || keyboard.started('w')) move = '+y';
-		else if (keyboard.started('down') || keyboard.started('s')) move = '-y';
-		else if (keyboard.started('left') || keyboard.started('a')) move = '-x';
-		else if (keyboard.started('right') || keyboard.started('d')) move = '+x';
-		else move = null;
+		var dir = getVectorOfMovement();
+		if (dir == null) {
+			this.object.properties['move'] = '';
+			return;
+		}
+
+		var move = getCardinalDirection(dir);
 		this.object.properties['move'] = move;
+	}
+
+	function getVectorOfMovement():Vec4 {
+		var keyboard = Input.getKeyboard();
+		if (keyboard.started('up') || keyboard.started('w'))
+			return object.transform.look();
+		else if (keyboard.started('down') || keyboard.started('s'))
+			return negate(object.transform.look());
+		else if (keyboard.started('left') || keyboard.started('a'))
+			return negate(object.transform.right());
+		else if (keyboard.started('right') || keyboard.started('d'))
+			return object.transform.right();
+		return null;
+	}
+
+	function getCardinalDirection(dir:Vec4):String {
+		if (dir.dot(Vec4.xAxis()) > 0.7) return '+x';
+		else if (dir.dot(Vec4.xAxis()) < -0.7) return '-x';
+		else if (dir.dot(Vec4.yAxis()) > 0.7) return '+y';
+		else if (dir.dot(Vec4.yAxis()) < -0.7) return '-y';
+		else if (dir.dot(Vec4.zAxis()) > 0.7) return '+z';
+		else if (dir.dot(Vec4.zAxis()) < -0.7) return '-z';
+		return '';
+	}
+
+	function negate(vec:Vec4):Vec4 {
+		return new Vec4(-vec.x, -vec.y, -vec.z, -vec.w);
 	}
 }
